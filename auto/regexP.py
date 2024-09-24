67c9977f5abff7826373ee98a2742b620bad0a37
Hamza Siddiqui <hamza@technomites.com>
2024-09-24 18:56:14 +0500

    .

--- auto/regexP.py
+++ auto/regexP.py
@@ -7,6 +7,13 @@ emailRegex = re.compile(r'''(
     (\.[a-zA-Z]{2,4}) # dot-something
 )''', re.VERBOSE)
 
-pyperclip.copy('My name is Hamza')
-x = pyperclip.paste()
-print(x)
+# x = emailRegex.findall('')
+# print(x)
+emails= "In the bustling world of technology, many individuals are making their mark. For instance, you might find James Smith at james.smith123@example.com, while his colleague, Sarah Johnson, can be reached at sarah.johnson456@domain.com. Over in marketing, Mike Brown shares insights through mike.brown789@webmail.com, and Lisa White connects with clients at lisa.white2023@mailservice.org. Meanwhile, in the development team, you’ll often see Alex Green emailing updates from alex.green.dev@programming.com and Jessica Blue, who frequently collaborates with partners via jessica.blue@partnershub.net. Don't forget about Emily Clark, who shares her innovative ideas at emily.clark@creativeworks.com, or Robert Taylor, who updates everyone at robert.taylor@businessmail.com. Last but not least, you can also contact David Harris at david.harris2024@startupmail.com and Karen Lewis at karen.lewis@techgroup.org for more insights."
+
+matches = []
+
+for groups in emailRegex.findall(emails):
+    matches.append(groups[0])
+
+print(matches)
