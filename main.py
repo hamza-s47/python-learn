115edacda00bb55d5441858f5d83d05204a3ac6e
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-10-23 19:01:07 +0500

    .

---
+++ main.py
@@ -0,0 +1,16 @@
+import shutil, pprint, os, shelve
+
+# cats = ['Zophie', 'Pooka', 'Simon']
+# with open('test.txt', 'w') as file:
+#     file.write('Hello world!')
+
+# abc = open('test.txt', 'w')
+# abc.write('I am learnig py again')
+# print(abc.read())
+# abc.close()
+
+# shelfFile = shelve.open('mydata',)
+# print(shelfFile['cat'])
+# shelfFile.close()
+
+os.remove('mydata.db')
